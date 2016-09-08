5361bda4d0966fb384c9e5e780d99df5bda396b6
Katie Nichols <Kaylotura@gmail.com>
2016-09-08 14:10:42 -0700

    Start Page includes all basic elements, and registered PlayerStats to admin

--- buster_world/models.py
+++ buster_world/models.py
@@ -3,7 +3,7 @@
 from django.db import models
 
 
-class PlayerStats:
+class PlayerStats(models.Model):
     """
     Value type that represents a player's name, score, and time played for any given session of the game.
     """
@@ -12,7 +12,7 @@ class PlayerStats:
     time = models.TextField()
 
     def __str__(self):
-        return self.name + '' + self.score
+        return self.name + ' ' + self.score
 
     def __repr__(self):
         return 'PlayerStats(name={!r}, score={!r}, time={!r})'.format(self.text, self.score, self.time)
