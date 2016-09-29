c352b97b9946badcd52492d3bb8bdf42a86d6a6b
Katie Nichols <Kaylotura@gmail.com>
2016-09-29 14:58:17 -0700

    changing buster_world to busterworld

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "buster_world.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "busterworld.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
